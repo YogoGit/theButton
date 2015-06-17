55d34da54a731d351fcd28b8763fa98c1ec69384
Luke Welna <lwelna@sofi.org>
2015-06-17 10:10:31 -0600

    add better tests, put each test in their own method, only use expeceted = exception when you acutally expect it and not all willy nilly

--- test/services/UserServiceTest.java
+++ test/services/UserServiceTest.java
@@ -19,57 +19,57 @@ public class UserServiceTest extends AbstractTransactionalJUnit4SpringContextTes
     @Autowired
     private UserService userService;
 
-    @Test(expected = java.lang.NoClassDefFoundError.class)
-    public void addUsernameTest() {
-        User user0 = new User();
-        User user1 = new User();
-        User user2 = new User();
-
-        user0.setUsername("user0");
-        userService.addUser(user0);
-        assertThat(userService.userExists(user0.getUsername())).isEqualTo(true);
-
-        // a username > 20 chars should not be added to the database
-        user1.setUsername("user1WhichIsLongerThan20Characters");
-        userService.addUser(user1);
-        assertThat(userService.userExists(user1.getUsername())).isEqualTo(false);
-
-        // a username < 3 chars should not be added to the database
-        user2.setUsername("u2");
-        userService.addUser(user2);
-        assertThat(userService.userExists(user2.getUsername())).isEqualTo(false);
+    @Test
+    public void addValidUsername() {
+        User user = new User();
+        user.setUsername("user0");
+        userService.addUser(user);
+        assertThat(userService.userExists(user.getUsername())).isEqualTo(true);
     }
 
-    @Test(expected = javax.persistence.EntityExistsException.class)
-    public void uniqueUsernameTest() {
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void addUsernameLong() {
+        User user = new User();
+        user.setUsername("user1WhichIsLongerThan20Characters");
+        userService.addUser(user);
+        assertThat(userService.userExists(user.getUsername())).isEqualTo(false);
+    }
+
+    @Test(expected = javax.validation.ConstraintViolationException.class)
+    public void addUsernameShort() {
+        User user = new User();
+        user.setUsername("u2");
+        userService.addUser(user);
+        assertThat(userService.userExists(user.getUsername())).isEqualTo(false);
+    }
+
+    @Test
+    public void userDoesNotExist() {
+        assertThat(userService.userExists("user")).isEqualTo(false);
+    }
+
+    @Test
+    public void addingMultipleUsers() {
         User user0 = new User();
         User user1 = new User();
 
         user0.setUsername("user0");
         userService.addUser(user0);
         assertThat(userService.userExists(user0.getUsername())).isEqualTo(true);
-
-        user1.setUsername("user0");
-        userService.addUser(user0);
-        assertThat(userService.userExists(user0.getUsername())).isEqualTo(false);
 
         user1.setUsername("user1");
         userService.addUser(user1);
         assertThat(userService.userExists(user1.getUsername())).isEqualTo(true);
     }
 
-    @Test(expected = javax.persistence.EntityExistsException.class)
-    public void getUserData() {
-        User user0 = new User();
-        user0.setUsername("user0");
-        userService.addUser(user0);
-
-        User user1 = new User();
-        user1.setUsername("user0");
-        userService.addUser(user1);
-
-        assertThat(userService.getUserData(user0.getUsername())).isNotNull();
-        assertThat(userService.getUserData(user1.getUsername())).isNull();
+    @Test
+    public void UserDataNotExist() {
         assertThat(userService.getUserData("BobMarley")).isNull();
+    }
+
+    @Test
+    public void notInitializedUser() {
+        User user = new User();
+        assertThat(userService.addUser(user)).isEqualTo(false);
     }
 }
