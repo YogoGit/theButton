0690e7fa7675a6be5a3f7def2493e2bffd91c157
Luke Welna <lwelna@sofi.org>
2015-06-17 13:42:12 -0600

    add more tests to check more edge cases

--- test/services/UserServiceTest.java
+++ test/services/UserServiceTest.java
@@ -27,20 +27,40 @@ public class UserServiceTest extends AbstractTransactionalJUnit4SpringContextTes
         assertThat(userService.userExists(user.getUsername())).isEqualTo(true);
     }
 
+    // the database does not allow data that is longer than 20 characters to be added
     @Test(expected = javax.validation.ConstraintViolationException.class)
     public void addUsernameLong() {
         User user = new User();
         user.setUsername("user1WhichIsLongerThan20Characters");
+        assertThat(user.getUsername().length()).isGreaterThan(20);
         userService.addUser(user);
         assertThat(userService.userExists(user.getUsername())).isEqualTo(false);
     }
 
+    // the database does not allow data that is shorter than 3 characters to be added
     @Test(expected = javax.validation.ConstraintViolationException.class)
     public void addUsernameShort() {
         User user = new User();
         user.setUsername("u2");
+        assertThat(user.getUsername().length()).isLessThan(3);
         userService.addUser(user);
         assertThat(userService.userExists(user.getUsername())).isEqualTo(false);
+    }
+
+    @Test
+    public void addNullUser() {
+        assertThat(userService.addUser(null)).isEqualTo(false);
+    }
+
+    @Test
+    public void notInitializedUser() {
+        User user = new User();
+        assertThat(userService.addUser(user)).isEqualTo(false);
+    }
+
+    @Test
+    public void nullUserExists() {
+        assertThat(userService.userExists(null)).isEqualTo(false);
     }
 
     @Test
@@ -51,25 +71,31 @@ public class UserServiceTest extends AbstractTransactionalJUnit4SpringContextTes
     @Test
     public void addingMultipleUsers() {
         User user0 = new User();
-        User user1 = new User();
-
         user0.setUsername("user0");
         userService.addUser(user0);
         assertThat(userService.userExists(user0.getUsername())).isEqualTo(true);
 
+        User user1 = new User();
         user1.setUsername("user1");
         userService.addUser(user1);
         assertThat(userService.userExists(user1.getUsername())).isEqualTo(true);
     }
 
     @Test
-    public void UserDataNotExist() {
+    public void userDataNotExist() {
         assertThat(userService.getUserData("BobMarley")).isNull();
     }
 
     @Test
-    public void notInitializedUser() {
+    public void nullUserDatat() {
+        assertThat(userService.getUserData(null)).isNull();
+    }
+
+    @Test
+    public void userDataExists() {
         User user = new User();
-        assertThat(userService.addUser(user)).isEqualTo(false);
+        user.setUsername("user0");
+        userService.addUser(user);
+        assertThat(userService.getUserData(user.getUsername())).isNotNull();
     }
 }
